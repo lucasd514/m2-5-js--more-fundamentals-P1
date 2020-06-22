7548c71fc3b1964958174ac8c9fd8e9ec67fe064
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-22 16:57:05 -0400

    q3

--- workshop/exercises-P3_problems/exercise-3-1/app.js
+++ workshop/exercises-P3_problems/exercise-3-1/app.js
@@ -6,3 +6,28 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+const container = document.querySelector("#btn-list");
+
+function onClick(event) {
+  const btnID = event.target.id;
+  console.log(btnID);
+  switch (btnID) {
+    case "btn-1":
+      event.target.style.opacity = "0";
+      break;
+    case "btn-2":
+      event.target.style.backgroundColor = "rgb(240,188,66)";
+      break;
+    case "btn-3":
+      event.target.style.backgroundColor = "rgb(134,38,51)";
+      break;
+    case "btn-4":
+      event.target.classList.add("jitters");
+      break;
+    default:
+      return "wrong click homey";
+      break;
+  }
+}
+
+container.addEventListener("mousedown", onClick);
