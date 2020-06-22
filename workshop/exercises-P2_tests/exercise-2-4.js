a3244688d2224c28eccfcf35a0e5612af0c17fd7
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-22 15:02:50 -0400

    2.4.6

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,12 +10,22 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  let phrase = str;
+  let position = str.charAt(index);
+  if (str.charAt(index) === "") return undefined;
+  else console.log(position);
+  return position;
 }
 
 // Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
+expect(getLetterAtIndex("Ciao", 0), "C");
+expect(getLetterAtIndex("bello", 0), "b");
+expect(getLetterAtIndex("tutt", 3), "t");
+expect(getLetterAtIndex("appost", 0), "a");
+expect(getLetterAtIndex("sisisisisis", 76), undefined);
+expect(getLetterAtIndex("123456789", 8), "9");
 
 /**
  * -------------------------------------------------------------------
@@ -24,7 +34,7 @@ expect(getLetterAtIndex('goodbye', 0), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
