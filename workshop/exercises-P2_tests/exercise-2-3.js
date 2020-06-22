6a15835601846966bc45878749ff1a8a59ae8cbd
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-22 14:15:37 -0400

    2.3

--- workshop/exercises-P2_tests/exercise-2-3.js
+++ workshop/exercises-P2_tests/exercise-2-3.js
@@ -8,13 +8,20 @@
 // HINT: You can use the typeof function to check the type of each element:
 //   - `typeof 3` returns 'number')
 
-function sumDigits(input) {
-  // Your code here
+function sumDigits(i) {
+  if (i.length === 2 && typeof i[0] === "number" && typeof i[1] === "number")
+    return i[0] + i[1];
+  else return undefined;
 }
 
 // Add 5 more test cases here! 7 total
 expect(sumDigits([2, 5]), 7);
 expect(sumDigits([-1, 0]), -1);
+expect(sumDigits([-1, 1, 4]), undefined);
+expect(sumDigits("red"), undefined);
+expect(sumDigits([10220, 1]), 10221);
+expect(sumDigits([44, 44]), 88);
+expect(sumDigits([-1, 1, "black"]), undefined);
 
 /**
  * -------------------------------------------------------------------
@@ -23,7 +30,7 @@ expect(sumDigits([-1, 0]), -1);
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
